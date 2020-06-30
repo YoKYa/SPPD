d37c94a54c9cfdd8a30237c7b11abadf73b08ed5
YoKYa <yogiekaprastiya1@gmail.com>
2020-06-30 09:09:45 +0700

    second

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,7 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}" style="font-weight:bold;">
-                    {{ config('app.name', 'SPPD') }} DPU Jawa Timur
+                    {{ config('app.name', 'SPPD') }} DPU SDA Jawa Timur
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse"
                     data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
